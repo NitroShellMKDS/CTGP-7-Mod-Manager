670f73c2465a0eb6efc1dd9e5236f4acb4fd63b0
Brett Bergstrom <bonkmaykr@screwgravity.net>
2026-07-22 00:01:50 -0500

    Remove redundant log path, Whoops

--- source/main.c
+++ source/main.c
@@ -23,7 +23,6 @@ static const int CATEGORIES[] = {35931, 10605, 35932, 35943, 35933, 35935, 35937
 static const int NUM_CATEGORIES = sizeof(CATEGORIES) / sizeof(CATEGORIES[0]);
 
 static const bool log_to_file = false;
-#define log_loc "sdmc:/3ds/CTGP-7-Mod-Manager/output.log"
 
 #define MAX_PATH 512
 #define MAX_URL 2048
@@ -442,8 +441,8 @@ int main(int argc, char **argv) {
     consoleInit(GFX_TOP, NULL);
     printf("\x1b[1;1HGamebananaFetcher 3DS Port\n");
     if (log_to_file) {
-        printf("Logging to " log_loc "\n");
-        freopen(log_loc, "w", stdout);
+        printf("Logging to %s\n", LOG_FILE);
+        freopen(LOG_FILE, "w", stdout);
     }
 
     socBuffer = (u32*)memalign(SOC_ALIGN, SOC_BUFFERSIZE);
