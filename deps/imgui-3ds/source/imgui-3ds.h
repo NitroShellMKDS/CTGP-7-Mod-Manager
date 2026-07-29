74e4d63182f256d7257f6b461c3a3d5d2e03d14b
Brett Bergstrom <bonkmaykr@screwgravity.net>
2026-07-29 04:06:55 -0500

    Recommit imgui-3ds as part of filesystem

---
+++ deps/imgui-3ds/source/imgui-3ds.h
@@ -0,0 +1,12 @@
+//
+// Created by bonkyboo on 7/29/26.
+//
+
+#ifndef CTGP7MODMANAGER_IMGUI_3DS_H
+#define CTGP7MODMANAGER_IMGUI_3DS_H
+
+extern void sceneInit(void);
+extern void sceneRender(void);
+extern void sceneExit(void);
+
+#endif //CTGP7MODMANAGER_IMGUI_3DS_H
